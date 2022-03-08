5f1f1a18a6c39b2bac364e465fc3d2e53692dbd7
Benjamin <17143@Benjamin91.(none)>
2022-03-08 16:16:55 -0700

    Adding current progress to separate branch

--- testPage.test.ts
+++ testPage.test.ts
@@ -1,1 +1,17 @@
-//as
+import { JCP } from "./jcpPage";
+
+const jcp = new JCP;
+
+
+//Benjamin's Tests
+test("Search Bar Test", async () => {
+    await jcp.navigate();
+    await jcp.search("pants");
+    expect(await jcp.getResults()).toContain("pants")
+})
+
+
+test("Log In User", async () => {
+    await jcp.navigate();
+    await jcp.click(jcp.myAccount)
+})
