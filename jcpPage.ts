f0bc8a02db3a6ab41090cc18e75aa68b41c1712d
Benjamin <17143@Benjamin91.(none)>
2022-03-07 18:27:40 -0700

    Adding Web Selectors to Page Objects

--- jcpPage.ts
+++ jcpPage.ts
@@ -2,7 +2,22 @@ import {By} from "selenium-webdriver"
 import { BasePage } from "./basePage"
 
 export class JCP extends BasePage {
-    searchBar: By = By.name("q");
+
+    //Benjamin's elements: Search, Location Selector, Login/Register
+    searchBar: By = By.xpath('//input[@type="search"]')
+    locationSelector: By = By.xpath('//div[@id="findStore"]')
+    locationSearchBar: By = By.xpath('//input[@class="search-input"]')
+    locationSearchButton: By = By.xpath('//button[@type="submit"]')
+    myAccount: By = By.xpath('//div[@class="_1E7u2 _3w1jh"]')
+    loginEmail: By = By.xpath('//input[@id="loginEmail"]')
+    loginPassword: By = By.xpath('//input[@data-automation-id="passwrod_input"]')
+    signInButton: By = By.xpath('//button[@data-automation-id="signin_button"]')
+    createAccountButton: By = By.xpath('//button[@data-automation-id="create_account_button"]')
+
+    //Clarissa's elements:
+    
+    //Zane's elements: 
+
     results: By = By.id('rso')
     constructor() {
         super({url: "https://www.jcpenney.com/"});
